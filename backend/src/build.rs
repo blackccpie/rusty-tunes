b1790dbc1e41b5af47710cef4f0c12b240ecd20a
blackccpie <16404298+blackccpie@users.noreply.github.com>
2024-01-01 18:24:53 +0100

    add : initial frontend/backend files.

---
+++ backend/src/build.rs
@@ -0,0 +1,42 @@
+/*
+The MIT License
+
+Copyright (c) 2024 Albert Murienne
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+use std::path::Path; 
+use std::process::Command;
+
+const FRONTEND_DIR: &str = "../frontend";
+
+fn main() {
+    println!("cargo:rerun-if-changed={}/src", FRONTEND_DIR);
+    println!("cargo:rerun-if-changed={}/index.html", FRONTEND_DIR);
+    build_frontend(FRONTEND_DIR);
+}
+
+fn build_frontend<P: AsRef<Path>>(source: P) {
+    Command::new("trunk")
+        .args(&["build", "--release"])
+        .current_dir(source.as_ref())
+        .status()
+        .expect("Failed to build Frontend");
+}
---
+++ backend/src/main.rs
@@ -0,0 +1,59 @@
+/*
+The MIT License
+
+Copyright (c) 2024 Albert Murienne
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#[macro_use] extern crate rocket;
+
+use rocket::fs::NamedFile;
+use rocket::response::status::NotFound;
+use std::path::PathBuf;
+
+// return the index file as a Rocket NamedFile
+async fn get_index() -> Result<NamedFile, NotFound<String>> {
+    NamedFile::open("../frontend/dist/index.html")
+        .await
+        .map_err(|e| NotFound(e.to_string()))
+}
+
+// create a route for any url that is a path from the /
+#[get("/<path..>")]
+async fn static_files(path: PathBuf) -> Result<NamedFile, NotFound<String>> {
+    let path = PathBuf::from("../frontend/dist").join(path);
+    match NamedFile::open(path).await {
+        Ok(f) => Ok(f),
+        Err(_) => get_index().await,
+    }
+}
+
+// return the index when the url is /
+#[get("/")]
+async fn index() -> Result<NamedFile, NotFound<String>> {
+    get_index().await
+}
+
+#[launch]
+fn rocket() -> _ {
+    // you must mount the static_files route
+    rocket::build()
+        .mount("/", routes![index, static_files])
+}
