d208e66d2d805cc8e3aeeecb5e8b5ff3d1430295
blackccpie <16404298+blackccpie@users.noreply.github.com>
2024-01-27 22:14:04 +0100

    feat : randomization now working in wasm.

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -23,12 +23,14 @@ THE SOFTWARE.
 */
 
 extern crate core;
-use core::deezer_wrapper;
+use core::parser::Track;
 
 mod fetchstates; // TODO : change modules architectures accordingly to rust book...
 
 mod xmlplist;
 use crate::xmlplist::XmlPlist;
+
+use rand::seq::SliceRandom;
 
 use std::path::PathBuf;
 
@@ -37,23 +39,51 @@ use yew::prelude::*;
 #[function_component(App)]
 fn app() -> Html {
 
-    let onclick = Callback::from(move |_| {
+    let itunes_tracks = use_state(|| None::<Vec<Track>>);
+    let track_name = use_state(|| "Uninitialized".to_string());
+
+    // TODO : little code cleanup...
+    let onload = {
+        let itunes_tracks = itunes_tracks.clone();
+        Callback::from(move |_| {
             use web_sys::console;
             let file_uri : String = "/api/xml/itunes_library_redux.xml".to_string();
-            wasm_bindgen_futures::spawn_local(async move {
-                let body = reqwasm::http::Request::get(file_uri.as_str())
-                    .send().await.unwrap().json::<std::collections::HashMap<String,String>>().await.unwrap();
-                console::log_1(&body["xml_plist"].clone().into());
-                let itunes_library = core::parser::parse_xmlbytes_plist(&body["xml_plist"].clone().into_bytes());
+            wasm_bindgen_futures::spawn_local({
+                let itunes_tracks = itunes_tracks.clone();
+                async move {
+                    let body = reqwasm::http::Request::get(file_uri.as_str())
+                        .send().await.unwrap().json::<std::collections::HashMap<String,String>>().await.unwrap();
+                    console::log_1(&body["xml_plist"].clone().into());
+                    let library = core::parser::parse_xmlbytes_plist(&body["xml_plist"].clone().into_bytes());
+                    let tracks : Vec<Track> = library.Tracks.values().cloned().collect::<Vec<Track>>();
+                    itunes_tracks.set( Some(tracks) );
+                }
             });
             let message = String::from("xml plist loaded!");
             console::log_1(&message.into());
-        });
+        })
+    };
+
+    let onrand = {
+        let itunes_tracks = itunes_tracks.clone();
+        let track_name = track_name.clone();
+        Callback::from(move |_| {
+            let rand_track = itunes_tracks.as_ref().unwrap().choose(&mut rand::thread_rng()).unwrap().clone();
+            use web_sys::console;
+            track_name.set(rand_track.Name.clone());
+            console::log_1(&rand_track.Name.into());
+        })
+    };
 
     html! {
         <main>
         <h1>{ "Hello World" }</h1>
-        <button {onclick} class="button button-primary">{"Load"}</button>
+        <button onclick={onload} class="button button-primary">{"Load"}</button>
+        <button onclick={onrand} class="button button-primary">{"Randomize"}</button>
+        <p>
+            <b>{ "Current track: " }</b>
+            { (*track_name).clone() }
+        </p>
         //<div class="container-sm justify-content-center m-5">
         //    <XmlPlist id={"itunes_library_redux.xml"}/>
         //</div>
